68b1946ac51529d8792f6db6f3c443fdb0a77e92
cchampou <clement@champouillon.com>
2020-11-03 05:06:04 +0100

    With all icons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 export { default as Anemo } from './icons/anemo.js';
+export { default as Cryo } from './icons/cryo.js';
+export { default as Dendro } from './icons/dendro';
+export { default as Electro } from './icons/electro';
+export { default as Geo } from './icons/geo';
+export { default as Hydro } from './icons/hydro';
+export { default as Pyro } from './icons/pyro';
