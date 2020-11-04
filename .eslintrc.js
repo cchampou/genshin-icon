16aac63d83f3e4ef3cae102a71e99d3e7c65def5
cchampou <clement@champouillon.com>
2020-11-04 01:43:02 +0100

    With eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,40 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+    "extends": "airbnb",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".tsx", ".ts"] }],
+        "react/jsx-props-no-spreading": 0,
+        "import/extensions": ["error", "ignorePackages", {
+            js: "never",
+            mjs: "never",
+            jsx: "never",
+            ts: "never",
+            tsx: "never",
+        }],
+    },
+    "settings": {
+        "import/resolver": {
+            node: {
+                extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+            }
+        },
+    },
 };
